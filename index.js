daeffd500e2ac83adb241d6f5c0fe8fad35eabf4
cabraja <cabrajamihajlo@gmail.com>
2021-11-29 13:16:24 +0100

    added slider buttons

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const sliderContainer = document.querySelector("#slider-container");
 const slides = document.querySelectorAll(".slide");
+const slideButtons = document.querySelectorAll(".slide-button");
 let slidePercent = 0;
 
 setInterval(() => {
     slidePercent += 100;
 
     if(slidePercent < 400){
-        slides.forEach(slide => {
-            slide.style.transform = `translateX(-${slidePercent}%)`;   
+        slides.forEach((slide,index) => {
+            slide.style.transform = `translateX(-${slidePercent}%)`; 
         })
+        slideButtons.forEach(button => {
+            button.classList.remove("active");
+        })
+        slideButtons[slidePercent/100].classList.add("active");
 
     }else{
         slidePercent = 0;
-        slides.forEach(slide => {
-            slide.style.transform = `translateX(-${slidePercent}%)`;   
+        slides.forEach((slide,index) => {
+            slide.style.transform = `translateX(-${slidePercent}%)`;
         })
+        slideButtons.forEach(button => {
+            button.classList.remove("active");
+        })
+        slideButtons[slidePercent/100].classList.add("active");
     }
-}, 4000);
+}, 5000);
